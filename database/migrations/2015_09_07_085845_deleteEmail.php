eb5c18587c7b61596618d7f6cdb4dcddd0cca099
Insane <luojieyy@gmail.com>
2015-09-07 17:03:48 +0800

    migrate

---
+++ database/migrations/2015_09_07_085845_deleteEmail.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteEmail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
